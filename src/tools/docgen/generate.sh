58da86ee58083809f14ebeb29ed4763f7ea49b53
spirali <>
2007-07-27 11:11:13 +0100

    Small update of doc2html

--- src/tools/docgen/generate.sh
+++ src/tools/docgen/generate.sh
@@ -5,4 +5,4 @@ export KREATRIX_MODULES_PATH=../../modules
 cd `dirname $0`
 rm ../../../docs/html/*.html
 ../../kreatrix grabdoc.kx ../../*.c
-../../kreatrix -d doc2html.kx Lobby sockets system pool cgi test threads xml
+../../kreatrix -d doc2html.kx Lobby sockets system pool cgi test threads xml sdl 
