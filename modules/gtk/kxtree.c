e2cd6b9020ea156f77ec1ae0b883fbf5f911f9d5
spirali <>
2007-11-21 21:19:39 +0100

    Added missing source files into module gtk

---
+++ modules/gtk/kxtree.c
@@ -0,0 +1,352 @@
+#include "kxtree.h"
+
+#include <stdlib.h>
+
+#include "kxmessage.h"
+#include "kxobject.h"
+#include "kxcfunction.h"
+#include "kxinteger.h"
+#include "kxfloat.h"
+#include "kxstring.h"
+#include "kxexception.h"
+#include "kxlist.h"
+#include "gtk_utils.h"
+#include "gtktreeiter.h"
+#include "gtktreemodel.h"
+#include "gtktreeview.h"
+
+static KxObject *
+kxgtkliststore_clone_with_columns (KxObject *self, KxMessage *message)
+{
+	KXPARAM_TO_LIST(list, 0);
+
+	int t;
+	int columns = list->size;
+
+	GType *types = malloc(sizeof(GType) * columns);
+	ALLOCTEST(types);
+	
+	for (t=0;t<list->size;t++) {
+		types[t] = kxgtk_object_to_gtype(list->items[t]);
+	}
+
+	KxObject *clone = kxobject_raw_clone(self);
+	clone->data.ptr = gtk_list_store_newv(columns, types);
+	kxgtk_set_wrapper(clone, clone->data.ptr);
+	g_object_unref(clone->data.ptr);
+
+	free(types);
+	
+	return clone;
+}
+
+static KxObject *
+kxgtkliststore_at_set_value_column (KxObject *self, KxMessage *message) 
+{
+	KXPARAM_TO_GTKTREEITER(iter, 0);
+	KXPARAM_TO_INT(column, 1);
+	KxObject *obj = message->params[2];
+	
+	GValue value = { 0 };
+ 	g_value_init(&value,
+                 gtk_tree_model_get_column_type(GTK_TREE_MODEL(self->data.ptr),
+                 	column));
+	
+	if (kxgtk_gvalue_from_object(obj, &value)) {
+		KxObject *typestr = kxobject_type_name(obj);
+		KxException *excp = kxexception_new_with_text(KXCORE,
+			"Cannot convert '%s' to '%s'", 
+			KXSTRING_VALUE(typestr), 
+			g_type_name(G_TYPE_FUNDAMENTAL(G_VALUE_TYPE(&value))));
+		REF_REMOVE(typestr);
+		KXTHROW(excp);
+	}
+
+	gtk_list_store_set_value(GTK_LIST_STORE(self->data.ptr), iter, column, &value);
+
+	g_value_unset(&value);
+
+	KXRETURN(self);
+}
+
+static KxObject *
+kxgtkliststore_at_set_values (KxObject *self, KxMessage *message) 
+{
+	KXPARAM_TO_GTKTREEITER(iter, 0);
+	KXPARAM_TO_LIST(list, 1);
+
+	GValue value = { 0 };
+
+	int t;
+	for (t = 0; t < list->size; t++) {
+		KxObject *obj = list->items[t];
+	 	g_value_init(&value,
+    	             gtk_tree_model_get_column_type(GTK_TREE_MODEL(self->data.ptr),
+        	         	t));
+	
+		if (kxgtk_gvalue_from_object(obj, &value)) {
+			KxObject *typestr = kxobject_type_name(obj);
+			KxException *excp = kxexception_new_with_text(KXCORE,
+				"Cannot convert '%s' to '%s'", 
+				KXSTRING_VALUE(typestr), 
+				g_type_name(G_TYPE_FUNDAMENTAL(G_VALUE_TYPE(&value))));
+			REF_REMOVE(typestr);
+			KXTHROW(excp);
+		}
+		gtk_list_store_set_value(GTK_LIST_STORE(self->data.ptr), iter, t, &value);
+	}
+
+
+	g_value_unset(&value);
+	KXRETURN(self);
+}
+
+
+void kxgtkliststore_extra_init(KxObject *self) {
+	KxMethodTable table[] = {
+		{"cloneWithColumns:",1,kxgtkliststore_clone_with_columns},
+		{"at:column:setValue:",3, kxgtkliststore_at_set_value_column},
+		{"at:putValues:",2, kxgtkliststore_at_set_values },
+		{NULL,0, NULL}
+	};
+	kxobject_add_methods(self, table);
+}
+
+static KxObject *
+kxgtktreestore_clone_with_columns (KxObject *self, KxMessage *message)
+{
+	KXPARAM_TO_LIST(list, 0);
+
+	int t;
+	int columns = list->size;
+
+	GType *types = malloc(sizeof(GType) * columns);
+	ALLOCTEST(types);
+	
+	for (t=0;t<list->size;t++) {
+		types[t] = kxgtk_object_to_gtype(list->items[t]);
+	}
+
+	KxObject *clone = kxobject_raw_clone(self);
+	clone->data.ptr = gtk_tree_store_newv(columns, types);
+	kxgtk_set_wrapper(clone, clone->data.ptr);
+	g_object_unref(clone->data.ptr);
+
+	free(types);
+	
+	return clone;
+}
+
+static KxObject *
+kxgtktreestore_at_set_value_column (KxObject *self, KxMessage *message) 
+{
+	KXPARAM_TO_GTKTREEITER(iter, 0);
+	KXPARAM_TO_INT(column, 1);
+	KxObject *obj = message->params[2];
+	
+	GValue value = { 0 };
+ 	g_value_init(&value,
+                 gtk_tree_model_get_column_type(GTK_TREE_MODEL(self->data.ptr),
+                 	column));
+	
+	if (kxgtk_gvalue_from_object(obj, &value)) {
+		KxObject *typestr = kxobject_type_name(obj);
+		KxException *excp = kxexception_new_with_text(KXCORE,
+			"Cannot convert '%s' to '%s'", 
+			KXSTRING_VALUE(typestr), 
+			g_type_name(G_TYPE_FUNDAMENTAL(G_VALUE_TYPE(&value))));
+		REF_REMOVE(typestr);
+		KXTHROW(excp);
+	}
+
+	gtk_tree_store_set_value(GTK_TREE_STORE(self->data.ptr), iter, column, &value);
+
+	g_value_unset(&value);
+
+	KXRETURN(self);
+}
+
+static KxObject *
+kxgtktreestore_at_set_values (KxObject *self, KxMessage *message) 
+{
+	KXPARAM_TO_GTKTREEITER(iter, 0);
+	KXPARAM_TO_LIST(list, 1);
+
+	int t;
+
+
+
+	for (t = 0; t < list->size; t++) {
+		KxObject *obj = list->items[t];
+
+		GValue value = { 0 };
+
+	 	g_value_init(&value,
+    	             gtk_tree_model_get_column_type(GTK_TREE_MODEL(self->data.ptr),
+        	         	t));
+	
+		if (kxgtk_gvalue_from_object(obj, &value)) {
+			KxObject *typestr = kxobject_type_name(obj);
+			KxException *excp = kxexception_new_with_text(KXCORE,
+				"Cannot convert '%s' to '%s'", 
+				KXSTRING_VALUE(typestr), 
+				g_type_name(G_TYPE_FUNDAMENTAL(G_VALUE_TYPE(&value))));
+			REF_REMOVE(typestr);
+			KXTHROW(excp);
+		}
+		gtk_tree_store_set_value(GTK_TREE_STORE(self->data.ptr), iter, t, &value);
+
+		g_value_unset(&value);
+	}
+	KXRETURN(self);
+}
+
+
+void kxgtktreestore_extra_init(KxObject *self) {
+	KxMethodTable table[] = {
+		{"cloneWithColumns:",1,kxgtktreestore_clone_with_columns},
+		{"at:column:setValue:",3, kxgtktreestore_at_set_value_column},
+		{"at:putValues:",2, kxgtktreestore_at_set_values },
+		{NULL,0, NULL}
+	};
+	kxobject_add_methods(self, table);
+}
+
+static void
+kxgtk_mark_gobject_list_callback(GObject *self, gpointer data)
+{
+	kxgtk_mark_gobject(self);
+}
+
+void kxgtktreeview_extra_mark(KxObject *self) 
+{
+	GtkTreeView *data = self->data.ptr;
+	GtkTreeModel *model = gtk_tree_view_get_model(data);
+	if (model)
+		kxgtk_mark_gobject(G_OBJECT(model));
+	
+	GList *list = gtk_tree_view_get_columns(data);
+	g_list_foreach(list, (GFunc) kxgtk_mark_gobject_list_callback, NULL);
+}
+
+
+
+void kxgtktreeviewcolumn_extra_mark(KxObject *self) 
+{
+	GtkTreeViewColumn *data = self->data.ptr;
+	GList *list = gtk_tree_view_column_get_cell_renderers(data);
+	g_list_foreach(list, (GFunc) kxgtk_mark_gobject_list_callback, NULL);
+	g_list_free(list);
+}
+
+static kxgtktreemodel_extra_mark_callback(GtkTreeModel *model,
+                                          GtkTreePath *path,
+                                          GtkTreeIter *iter,
+                                          gpointer data)
+{
+	int *column = data;
+
+	GValue value = { 0 };
+	do {
+		gtk_tree_model_get_value(model, iter, *column, &value);
+
+		KxObject *self = g_value_get_boxed(&value);
+		if (self) {
+	//		printf("Mark! \n");
+	//		kxobject_dump(self);
+			kxobject_mark(self);
+		}
+		column++;
+		g_value_unset(&value);
+	} while( (*column) != -1);
+	return FALSE;
+}
+
+void kxgtktreemodel_extra_mark(KxObject *self) 
+{
+	GtkTreeModel *data = self->data.ptr;
+	int count = gtk_tree_model_get_n_columns(data);
+	int t;
+	
+	int columns_with_kxobjects[count+1];
+	int c = 0;
+
+	for (t=0; t < count; ++t) {
+		if (gtk_tree_model_get_column_type(data, t) == KX_TYPE_OBJECT) {
+				columns_with_kxobjects[c++] = t;	
+		}
+	}
+
+	if (c == 0)
+		return;
+	
+	columns_with_kxobjects[c] = -1;
+
+	gtk_tree_model_foreach(data, kxgtktreemodel_extra_mark_callback, columns_with_kxobjects);
+}
+
+static KxObject *
+kxgtktreeselection_selected_iter (KxObject *self, KxMessage *message)
+{
+	GtkTreeIter *iter = malloc(sizeof(GtkTreeIter));
+	gtk_tree_selection_get_selected(self->data.ptr, NULL, iter);
+	return KXGTKTREEITER(iter);
+
+}
+
+static KxObject *
+kxgtktreeselection_model (KxObject *self, KxMessage *message)
+{
+	GtkTreeModel *model;
+	gtk_tree_selection_get_selected(self->data.ptr, &model, NULL);
+	return KXGTKTREEMODEL(model);
+}
+
+static KxObject *
+kxgtktreeselection_is_selected (KxObject *self, KxMessage *message)
+{
+	return KXGBOOLEAN(gtk_tree_selection_get_selected(self->data.ptr, NULL, NULL));
+}
+
+
+void kxgtktreeselection_extra_init(KxObject *self) {
+
+	// Get selection from prototype of treeview 
+	self->data.ptr = 
+		gtk_tree_view_get_selection(
+			kxcore_get_prototype(KXCORE, &kxgtktreeview_extension)->data.ptr);
+	g_object_ref(self->data.ptr);
+	KxMethodTable table[] = {
+		{"selectedIter",0,kxgtktreeselection_selected_iter},
+		{"model",0,kxgtktreeselection_model},
+		{"isSelected",0,kxgtktreeselection_is_selected},
+		{NULL,0, NULL}
+	};
+	kxobject_add_methods(self, table);
+}
+
+static KxObject *
+kxgtktreemodel_value_at_column (KxObject *self, KxMessage *message)
+{
+	KXPARAM_TO_GTKTREEITER(iter, 0);
+	KXPARAM_TO_INT(column, 1);
+	GValue value = { 0 };
+	gtk_tree_model_get_value(self->data.ptr, iter, column, &value);
+	KxObject *obj = kxgtk_kxobject_from_gvalue(KXCORE, &value);
+	g_value_reset(&value);
+	return obj;
+}
+
+void kxgtktreemodel_extra_init(KxObject *self) {
+	
+	KxObjectExtension *ext = self->extension;
+	self->extension = NULL;
+
+	KxMethodTable table[] = {
+		{"valueAt:column:",2,kxgtktreemodel_value_at_column},
+		{NULL,0, NULL}
+	};
+	kxobject_add_methods(self, table);
+
+	self->extension = ext;
+}
---
+++ modules/gtk/kxtree.h
@@ -0,0 +1,6 @@
+#ifdef __KXGTK_KXTREE_H
+#define __KXGTK_KXTREE_H
+
+void kxgtkliststore_extra_init(KxObject *self);
+
+#endif
