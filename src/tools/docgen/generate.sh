1eac52802fd18983ab30c37f59bbabe5954665c1
Stanislav Bohm <spirali@kreatrix.org>
2008-07-02 15:25:35 +0200

    Documentation generator updated

--- src/tools/docgen/generate.sh
+++ src/tools/docgen/generate.sh
@@ -5,7 +5,7 @@ export KREATRIX_MODULES_PATH=../../../modules
 cd `dirname $0`
 rm ../../../docs/html/*.html
 ../../kreatrix grabdoc.kx ../../*.c
-../../kreatrix -d doc2html.kx Lobby sockets system pool cgi test threads xml sdl gtk random
+../../kreatrix -d doc2html.kx Lobby sockets system pool cgi test threads xml sdl gtk random bytecode time
 
 
 #../../kreatrix -dv doc2html.kx Lobby
