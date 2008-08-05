b1d917c0b7ad2d23533e67a5c6149642e5690bd0
Stanislav Bohm <spirali@kreatrix.org>
2008-08-05 11:54:25 +0200

    Added file COPYING and added header with licence into .c, .h and .kx files

--- src/kxactivation.h
+++ src/kxactivation.h
@@ -1,14 +1,24 @@
-/**********************************************************************
- *
- *  kxactivation.h - Activation of method or block
- *
- *	$Id$
- *
- *  KxActivation represents activation of KxCodeBlock 
- *  and inteprets bytecode from KxCodeBlock.
- *
- **********************************************************************/
+/*
+   kxactivation.h
+   Copyright (C) 2007, 2008  Stanislav Bohm
 
+   This file is part of Kreatrix.
+
+   Kreatrix is free software; you can redistribute it and/or modify it under
+   the terms of the GNU General Public License as published by the Free
+   Software Foundation; either version 2, or (at your option) any later
+   version.
+
+   Kreatrix is distributed in the hope that it will be useful, 
+   but WITHOUT ANY WARRANTY; without even the implied warranty 
+   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Kreatrix; see the file COPYING. If not, see 
+   <http://www.gnu.org/licenses/>.
+*/
+	
 #ifndef __KXACTIVATION_H
 #define __KXACTIVATION_H
 
