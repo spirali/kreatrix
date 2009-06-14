27a4427f46e14c49d03c7a78c8ad99043dcdd7ce
Stanislav Bohm <spirali@kreatrix.org>
2009-06-14 12:59:37 +0200

    Fixed problem with shell in benchmark's scripts

--- benchmarks/run_speedtest.sh
+++ benchmarks/run_speedtest.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cd `dirname %0`
 
 export KREATRIX_MODULES_PATH="../modules"
@@ -28,12 +30,12 @@ echo "Version ... `$KREATRIX -c "VM version println"`";
 echo "VM Supports ... `$KREATRIX -c "(VM supportList printString println)"`";
 
 echo "TEST: 1"
-sh $SCRIPT $KREATRIX $TEST_RUNS > tmp1
+bash $SCRIPT $KREATRIX $TEST_RUNS > tmp1
 cat tmp1
 
 for ((i=2;i<=MAIN_RUNS;i++)) do
 	echo "TEST: $i"
-	sh $SCRIPT $KREATRIX $TEST_RUNS > tmp2
+	bash $SCRIPT $KREATRIX $TEST_RUNS > tmp2
 	cat tmp2
 	$KREATRIX scripts/getmin.kx tmp1 tmp2 > tmp3
 	mv tmp3 tmp1
--- benchmarks/scripts/speed_test.sh
+++ benchmarks/scripts/speed_test.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 KREATRIX=$1
 SCRIPT=run.kx
